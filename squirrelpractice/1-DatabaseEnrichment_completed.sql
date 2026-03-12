930a5353a5bef7c85fd178d6f5e7e4faf4c68c14
Ian Fink <247514964+earthfink1982@users.noreply.github.com>
2026-03-12 12:54:31 -0500

    phase 1 completed

---
+++ squirrelpractice/1-DatabaseEnrichment_completed.sql
@@ -0,0 +1,14 @@
+ALTER TABLE sp_customers RENAME COLUMN id TO customer_id
+;
+ALTER TABLE sp_customers RENAME COLUMN j to contact_details
+;
+
+ALTER TABLE sp_sales 
+RENAME COLUMN id TO sale_id, 
+RENAME COLUMN d TO sale_date
+;
+
+
+ALTER TABLE sp_sales_rep 
+RENAME COLUMN id TO rep_id 
+;
