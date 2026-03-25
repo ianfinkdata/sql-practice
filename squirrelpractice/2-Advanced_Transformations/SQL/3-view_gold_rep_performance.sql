e41dc09c414167594503be9f73d4fd0e65705430
Ian Fink <247514964+earthfink1982@users.noreply.github.com>
2026-03-25 11:34:36 -0500

    added order by to chronologically sort by month and rep

--- squirrelpractice/2-Advanced_Transformations/SQL/3-view_gold_rep_performance.sql
+++ squirrelpractice/2-Advanced_Transformations/SQL/3-view_gold_rep_performance.sql
@@ -9,6 +9,6 @@ count(sale_id) as deals_closed,
 avg(sale_amount) as average_revenue 
 from silver_sales_pipeline
 group by rep_name, sale_month
+order by sale_month, rep_name
 )
 select *, SUM(total_revenue) OVER(partition by rep_name) as rep_cumulative_sales from monthlysummary;
-
