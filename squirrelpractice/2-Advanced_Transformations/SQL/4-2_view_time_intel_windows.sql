3bfe91210e86b1276ee0772b6cf0491a1f2a7e6f
Ian Fink <247514964+earthfink1982@users.noreply.github.com>
2026-03-27 14:27:58 -0500

    4-2 initial sql

---
+++ squirrelpractice/2-Advanced_Transformations/SQL/4-2_view_time_intel_windows.sql
@@ -0,0 +1,10 @@
+/* 
+Tier 2: The Analytical Approach
+Target View Name: vw_time_intel_windows
+Business Requirement: Create a view that returns a continuous monthly summary of the entire company's sales. For every month, show the current month's total revenue, the previous month's total revenue, and a Year-to-Date (YTD) cumulative total that resets every January 1st.
+Technical Constraints:
+You must accomplish both the prior-month retrieval and the YTD running total using Window Functions.
+Self-joins for the purpose of time comparison are strictly prohibited.
+
+*/
+
