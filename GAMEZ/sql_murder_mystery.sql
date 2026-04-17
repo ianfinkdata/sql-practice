d00522843d8aabe8ff6ca7bc6ab49eaf7b64c7a8
Ian Fink <247514964+ianfinkdata@users.noreply.github.com>
2026-04-16 22:19:11 -0500

    added base sql murder mystery query

---
+++ GAMEZ/sql_murder_mystery.sql
@@ -0,0 +1,16 @@
+
+
+/* ## Tables
+
+crime_scene_report as csr
+drivers_license as dl
+facebook_event_checkin as fb
+interview as inv
+get_fit_now_member as gfn_member
+get_fit_now_check_in as gfn_check_in
+solution as sol
+income as inc
+person as per
+
+*/
+
